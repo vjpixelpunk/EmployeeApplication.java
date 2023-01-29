7eb05ee725e105aed61527dcebcd26427cf2b642
mouli <vjpixelpunk@gmail.com>
2023-01-28 23:21:53 -0600

    Added fizzBuzz

---
+++ corejavalearning/src/main/java/FizzBuzz.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter a number : ");
+        int num = sc.nextInt();
+
+        if(num % 3 ==0 && num% 5 == 0){
+            System.out.println("FizzBuzz");
+        }else if (num % 3 == 0){
+            System.out.println("Fizz");
+        }else if (num % 5 ==0){
+            System.out.println("Buzz");
+        }else
+            System.out.println(num);
+    }
+}
